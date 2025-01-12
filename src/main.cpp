aa0888e94950979c0753a3afde8846dda9a0f862
ibrahimowins <ibrahimowins@gmail.com>
2025-01-12 15:05:22 +0100

    Planning to use token and id as env variables, for security and untracibility

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,5 @@
 #include "../include/main.hpp"
 
-#define TOKEN "7843656436:AAH6rROv5vGc2FiE16lGgD4-6A6OvbruW50" //odahimusbot
-
 #define TELEGRAM_BOT_API_URL "https://api.telegram.org/bot"
 
 
@@ -9,8 +7,7 @@ int main() {
  // Replace with your bot token
     using namespace trat;
 
-
-    int64_t chat_id = std::atoll(std::getenv("6479143326));                // Replace with your chat ID
+    int64_t chat_id = std::atoll(std::getenv("6479143326"));                // Replace with your chat ID
     const char* token_buffer =  std::getenv("7843656436:AAH6rROv5vGc2FiE16lGgD4-6A6OvbruW50");
     Bot bot = Bot(token_buffer, chat_id);
 
