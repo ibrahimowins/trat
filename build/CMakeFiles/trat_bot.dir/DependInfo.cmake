df1f24580039285a98ffaf933b3fe688d1856029
ibrahimowins <ibrahimowins@gmail.com>
2025-01-20 15:48:43 +0100

    Removed the scary Makefile

---
+++ build/CMakeFiles/trat_bot.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/odahim/Sources/GitHub/trat/src/Bot/bot.cpp" "CMakeFiles/trat_bot.dir/src/Bot/bot.cpp.o" "gcc" "CMakeFiles/trat_bot.dir/src/Bot/bot.cpp.o.d"
+  "/home/odahim/Sources/GitHub/trat/src/Bot/handling.cpp" "CMakeFiles/trat_bot.dir/src/Bot/handling.cpp.o" "gcc" "CMakeFiles/trat_bot.dir/src/Bot/handling.cpp.o.d"
+  "/home/odahim/Sources/GitHub/trat/src/Bot/listen.cpp" "CMakeFiles/trat_bot.dir/src/Bot/listen.cpp.o" "gcc" "CMakeFiles/trat_bot.dir/src/Bot/listen.cpp.o.d"
+  "/home/odahim/Sources/GitHub/trat/src/Bot/sending.cpp" "CMakeFiles/trat_bot.dir/src/Bot/sending.cpp.o" "gcc" "CMakeFiles/trat_bot.dir/src/Bot/sending.cpp.o.d"
+  "/home/odahim/Sources/GitHub/trat/src/Bot/telegramNetworking.cpp" "CMakeFiles/trat_bot.dir/src/Bot/telegramNetworking.cpp.o" "gcc" "CMakeFiles/trat_bot.dir/src/Bot/telegramNetworking.cpp.o.d"
+  "/home/odahim/Sources/GitHub/trat/src/Bot/work.cpp" "CMakeFiles/trat_bot.dir/src/Bot/work.cpp.o" "gcc" "CMakeFiles/trat_bot.dir/src/Bot/work.cpp.o.d"
+  "/home/odahim/Sources/GitHub/trat/src/Shell/shell.cpp" "CMakeFiles/trat_bot.dir/src/Shell/shell.cpp.o" "gcc" "CMakeFiles/trat_bot.dir/src/Shell/shell.cpp.o.d"
+  "/home/odahim/Sources/GitHub/trat/src/main.cpp" "CMakeFiles/trat_bot.dir/src/main.cpp.o" "gcc" "CMakeFiles/trat_bot.dir/src/main.cpp.o.d"
+  "/home/odahim/Sources/GitHub/trat/src/networking.cpp" "CMakeFiles/trat_bot.dir/src/networking.cpp.o" "gcc" "CMakeFiles/trat_bot.dir/src/networking.cpp.o.d"
+  "/home/odahim/Sources/GitHub/trat/src/parser.cpp" "CMakeFiles/trat_bot.dir/src/parser.cpp.o" "gcc" "CMakeFiles/trat_bot.dir/src/parser.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
